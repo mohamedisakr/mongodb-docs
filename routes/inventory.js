8fa2ebf2076f62dc92b16975d1692020ca93f809
mohamedisakr <mohamedisakr@gmail.com>
2021-07-10 17:37:23 +0200

    refactoring inventory route

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,20 +1,24 @@
 const router = require("express").Router();
 const { create, findAll, deleteOne } = require("../controllers/inventory");
 
-router.get("/", findAll);
+router.route("/").get(findAll).post(create);
+router.route("/:id").delete(deleteOne);
+
+module.exports = router;
+
+// router.get("/", findAll);
 
 // Retrieve a single Student with id
 // router.get("/:id", inventory.findOne);
 
 // Create a new Student
-router.post("/", create);
+// router.post("/", create);
 
 // Update a Student with id
 //   router.put("/:id", inventory.update);
 
 // Delete a Student with id
-router.delete("/:id", deleteOne);
+// router.delete("/:id", deleteOne);
 
 // Delete all inventory
 //   router.delete("/", inventory.deleteAll);
-module.exports = router;
